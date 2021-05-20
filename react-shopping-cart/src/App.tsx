213a934ffdf8f30c3402c3dbe730fc07c298a1b4
Vibhu Agrawal <vibhu1.agrawal@gmail.com>
2021-05-20 16:17:12 -0600

    Completed styling for items display

--- react-shopping-cart/src/App.tsx
+++ react-shopping-cart/src/App.tsx
@@ -8,7 +8,7 @@ import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 // styles
-import { Wrapper } from './App.styles';
+import { Wrapper, StyledButton } from './App.styles';
 // Types
 export type CartItemType = {
   id: number;
@@ -24,13 +24,17 @@ const getProducts = async (): Promise<CartItemType[]> => /* gets fake store data
   await(await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     'products', 
     getProducts
   );
   console.log(data);
 
-  const getTotalItems = () => null;
+  const getTotalItems = (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount, 0);
+  /* accumulator keeps track of number of items across iterations */
   
   const handleAddToCart = (clickedItem: CartItemType) => null;
 
@@ -41,6 +45,14 @@ const App = () => {
 
   return (
     <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart Goes Here
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map(item => (
           <Grid item key={item.id} xs={12} sm={4}>
